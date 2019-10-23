7bd0f509a980f9f5ab79511b3c2c2b0dc75d426e
pierre salmi <pierre.salmi@ynov.com>
2019-10-23 10:15:09 +0200

    1 fini

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -7,7 +7,23 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
-
+    let argent
+    let place = input[0]
+    for(let i=1; i < input.length; i++){
+      aled = input[i].split(' ')
+      place += parseInt(aled[0])
+      place -= parseInt(aled[1])
+    } 
+    if(place <= 100){
+      argent = 1000
+    }
+    else if(place <= 10000){
+      argent = 100
+    }
+    else{
+      argent = 'KO'
+    }
+    return argent
     // AND HERE
   },
   verify: function (dataset, output) {
